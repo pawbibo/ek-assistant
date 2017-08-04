9d9dd23e84602cbdec017d13f4b17d6b98d8ce12
pawbibo <jesus.cabelin@bibo.com.ph>
2017-08-04 11:01:19 +0800

    Add colors via chalk

--- get_diff.js
+++ get_diff.js
@@ -2,9 +2,9 @@ const git = require('simple-git')(`/var/www/html/eikaiwa-${process.argv[2]}`);
 const chalk = require('chalk');
 
 git
-  .exec(() => console.log(chalk.redBright('Checking out to master...')))
+  .exec(() => console.log(`Checking out to ${chalk.red.bold('master')}...`))
   .checkout('master')
-  .exec(() => console.log('Done checking out to master. Starting to pull files...'))
+  .exec(() => console.log(`Done checking out to ${chalk.red.bold('master')}. Starting to pull files...`))
   .pull()
   .exec(() => console.log('Done updating. Fetching tags...'))
   .pull('origin', 'master', {'--tags': null})
@@ -12,11 +12,13 @@ git
     if (result) {
       const tags = result.all.sort();
       const latest_tag = tags[tags.length-1];
-      console.log(`Latest tag found: ${latest_tag}`);
+      console.log(`Latest tag found: ${chalk.white.bold(latest_tag)}`);
 
       git.diff(['--name-only', latest_tag], (err, data) => {
           const files = data.split('\n').filter((file) => file.indexOf('Test') == -1).join('\n');
-          console.log(files);
+          console.log(chalk.white.bold('\n-- File Difference --\n'));
+          console.log(chalk.white(files));
+          console.log(chalk.white.bold('-- End of diff --'))
       })
     }
   })
