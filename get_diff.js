ef7a64f5158be1f99fd61d90e058e271c452dd0c
pawbibo <jesus.cabelin@bibo.com.ph>
2017-10-10 17:14:27 +0800

    convert to promise

--- get_diff.js
+++ get_diff.js
@@ -1,24 +1,36 @@
-const git = require('simple-git')(`/var/www/html/eikaiwa-${process.argv[2]}`);
-const chalk = require('chalk');
 
-git
-  .exec(() => console.log(`Checking out to ${chalk.red.bold('master')}...`))
-  .checkout('master')
-  .exec(() => console.log(`Done checking out to ${chalk.red.bold('master')}. Starting to pull files...`))
-  .pull()
-  .exec(() => console.log('Done updating. Fetching tags...'))
-  .pull('origin', 'master', {'--tags': null})
-  .tags((err, result) => {
-    if (result) {
-      const tags = result.all.sort();
-      const latest_tag = tags[tags.length-1];
-      console.log(`Latest tag found: ${chalk.white.bold(latest_tag)}`);
+exports.get_diff = (proj) => {
+  return new Promise(function(resolve, reject) {
+    const git = require('simple-git')(`/Users/paw/Documents/projects/eikaiwa-${proj}`);
+    const chalk = require('chalk');
+    let message;
+    git
+      .exec(() => console.log(`Checking out to ${chalk.red.bold('master')}...`))
+      .checkout('master')
+      .exec(() => console.log(`Done checking out to ${chalk.red.bold('master')}. Starting to pull files...`))
+      .pull()
+      .exec(() => console.log('Done updating. Fetching tags...'))
+      .pull('origin', 'master', {'--tags': null})
+      .tags((err, result) => {
+        if (result) {
+          const tags = result.all.sort();
+          const latest_tag = tags[tags.length-1];
+          console.log(`Latest tag found: ${chalk.white.bold(latest_tag)}`);
 
-      git.diff(['--name-only', latest_tag], (err, data) => {
-          const files = data.split('\n').filter((file) => file.indexOf('Test') == -1).join('\n');
-          console.log(chalk.white.bold('\n-- File Difference --\n'));
-          console.log(chalk.white(files));
-          console.log(chalk.white.bold('-- End of diff --'))
-      })
-    }
-  })
+          git.diff(['--name-only', latest_tag], (err, data) => {
+              const files = data.split('\n').filter((file) => file.indexOf('Test') == -1).join('\n');
+              console.log(chalk.white.bold('\n-- File Difference --\n'));
+              console.log(chalk.white(files));
+              console.log(chalk.white.bold('-- End of diff --'))
+              message = '\n-- File Difference --\n';
+              message += files;
+              message += '-- End of diff --';
+              resolve(message);
+          })
+        }
+      });
+    });
+};
+
+
+
